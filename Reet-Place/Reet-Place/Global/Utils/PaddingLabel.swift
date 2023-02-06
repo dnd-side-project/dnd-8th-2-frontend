2134aacdde88270d82bfd61ff195281ac8bb16d7
Kim HeeJae <iowa329@gmail.com>
2023-02-07 00:40:04 +0900

    [ADD] Utils/Padding 커스텀라벨 구성

---
+++ Reet-Place/Reet-Place/Global/Utils/PaddingLabel.swift
@@ -0,0 +1,25 @@
+//
+//  PaddingLabel.swift
+//  Reet-Place
+//
+//  Created by Kim HeeJae on 2023/02/04.
+//
+
+import UIKit
+
+/// Padding 값이 설정가능한 UILabel
+class PaddingLabel: UILabel {
+
+    var edgeInset: UIEdgeInsets = .zero
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: edgeInset.top, left: edgeInset.left, bottom: edgeInset.bottom, right: edgeInset.right)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + edgeInset.left + edgeInset.right, height: size.height + edgeInset.top + edgeInset.bottom)
+    }
+    
+}
