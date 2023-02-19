c0f3c83bfb25b233ba89f0547c73aec0ea659ef0
Kim HeeJae <iowa329@gmail.com>
2023-02-19 23:35:13 +0900

    [FEAT] : 네이버지도 및 검색바 레이아웃 구성

--- Reet-Place/Reet-Place/Screen/Home/ViewController/HomeVC.swift
+++ Reet-Place/Reet-Place/Screen/Home/ViewController/HomeVC.swift
@@ -13,13 +13,24 @@ import RxCocoa
 import Then
 import SnapKit
 
+import NMapsMap
+
 class HomeVC: BaseViewController {
     
     // MARK: - UI components
     
-    let label = UILabel()
+    private let mapView = NMFMapView()
+    
+    private let searchTextField = UITextField()
         .then {
-            $0.text = "HomeVC"
+            $0.backgroundColor = .white
+            $0.font = .body1
+            $0.attributedPlaceholder = NSAttributedString(
+                string: "시/군/구로 검색",
+                attributes: [NSAttributedString.Key.foregroundColor: AssetColors.gray500]
+            )
+            $0.addLeftPadding(padding: 16)
+            $0.layer.cornerRadius = 8
         }
     
     // MARK: - Variables and Properties
@@ -28,14 +39,37 @@ class HomeVC: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
         
-        view.backgroundColor = .red
-        
-        view.addSubview(label)
-        label.snp.makeConstraints {
-            $0.center.equalTo(view)
-        }
+        configureLayout()
     }
     
     // MARK: - Functions
+}
+
+// MARK: - Layout
+
+extension HomeVC {
+    
+    private func configureLayout() {
+        view.addSubviews([mapView,
+                         searchTextField])
+        
+        
+        mapView.snp.makeConstraints {
+            $0.edges.equalTo(view)
+        }
+        
+        searchTextField.snp.makeConstraints {
+            $0.height.equalTo(44)
+            
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.left.equalTo(mapView.snp.left).offset(20)
+            $0.right.equalTo(mapView.snp.right).inset(20)
+        }
+    }
+    
 }
