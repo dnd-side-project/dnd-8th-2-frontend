97977862802b595812b25713323e744f7d72de85
kth1210 <k2hop1210@gmail.com>
2023-02-25 23:16:17 +0900

    [FEAT] : ReetBottomSheet 추가

---
+++ Reet-Place/Reet-Place/Screen/Shared/View/ReetBottomSheet/ReetBottomSheet.swift
@@ -0,0 +1,163 @@
+//
+//  ReetBottomSheet.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 2023/02/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ReetBottomSheet: BaseViewController {
+    
+    enum SheetStyle {
+        case h160
+        case h420
+        case h480
+    }
+    
+    let dimmedView = UIView()
+        .then {
+            $0.backgroundColor = AssetColors.gray900.withAlphaComponent(0.7)
+        }
+    
+    let bottomSheetView = UIView()
+        .then {
+            $0.backgroundColor = AssetColors.white
+            $0.layer.cornerRadius = 16.0
+            $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            $0.clipsToBounds = true
+        }
+    
+    let sheetBar = UIView()
+        .then {
+            $0.backgroundColor = AssetColors.gray300
+            $0.isUserInteractionEnabled = false
+        }
+    
+    var sheetStyle: SheetStyle = .h160
+    
+    var sheetHeight: CGFloat {
+        switch sheetStyle {
+        case .h160:
+            return 160.0
+        case .h420:
+            return 420.0
+        case .h480:
+            return 480.0
+        }
+    }
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showBottomSheet()
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureContentView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    
+    // MARK: - Functions
+    
+    func showBottomSheet() {
+        bottomSheetView.snp.updateConstraints {
+            $0.height.equalTo(sheetHeight)
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn) {
+            self.dimmedView.alpha = 0.7
+            
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func dimmedViewTapped(_ tapRecognizer: UITapGestureRecognizer) {
+        dismissBottomSheet()
+    }
+    
+    private func dismissBottomSheet() {
+        bottomSheetView.snp.updateConstraints {
+            $0.height.equalTo(0)
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn) {
+            self.dimmedView.alpha = 0.0
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            if self.presentingViewController != nil {
+                self.dismiss(animated: false)
+            }
+        }
+    }
+    
+}
+
+
+// MARK: - Configure
+
+extension ReetBottomSheet {
+    
+    func configureContentView() {
+        view.backgroundColor = UIColor.clear
+        
+        view.addSubviews([dimmedView, bottomSheetView])
+        bottomSheetView.addSubview(sheetBar)
+        
+        dimmedView.alpha = 0.0
+
+        let dimmedTap = UITapGestureRecognizer(target: self, action: #selector(dimmedViewTapped))
+        dimmedView.addGestureRecognizer(dimmedTap)
+        dimmedView.isUserInteractionEnabled = true
+        
+        configureShadow()
+    }
+    
+    func configureShadow() {
+        view.layer.shadowColor = CGColor(red: 23.0 / 255.0, green: 23.0 / 255.0, blue: 23.0 / 255.0, alpha: 1)
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowRadius = 16.0
+        view.layer.shadowOffset = CGSize(width: 0, height: -2.0)
+        view.layer.masksToBounds = false
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension ReetBottomSheet {
+    
+    func configureLayout() {
+        dimmedView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        bottomSheetView.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalToSuperview()
+            $0.height.equalTo(0)
+        }
+        
+        sheetBar.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(8)
+            $0.width.equalTo(32)
+            $0.height.equalTo(3)
+        }
+    }
+    
+}
