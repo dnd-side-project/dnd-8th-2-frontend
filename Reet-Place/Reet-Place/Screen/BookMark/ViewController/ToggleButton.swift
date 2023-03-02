32c710176205d43f7a260f7da40b9d05f4cce697
kth1210 <k2hop1210@gmail.com>
2023-03-02 19:14:22 +0900

    [CHORE] : ToggleButton 코드 수정

--- Reet-Place/Reet-Place/Screen/BookMark/ViewController/ToggleButton.swift
+++ Reet-Place/Reet-Place/Screen/BookMark/ViewController/ToggleButton.swift
@@ -11,6 +11,8 @@ import Then
 
 class ToggleButton: BaseView {
     
+    // MARK: - UI components
+    
     let stackView = UIStackView()
         .then {
             $0.spacing = 0.0
@@ -25,6 +27,7 @@ class ToggleButton: BaseView {
             $0.setTitle("★", for: .normal)
             $0.isSelected = true
         }
+    
     let twoStarBtn = UIButton()
         .then {
             $0.tag = 2
@@ -32,49 +35,36 @@ class ToggleButton: BaseView {
             $0.layer.borderWidth = 1.0
             $0.setTitle("★★", for: .normal)
         }
+    
     let threeStarBtn = UIButton()
         .then {
             $0.tag = 3
             $0.setTitle("★★★", for: .normal)
         }
     
+    
+    // MARK: - Variables and Properties
+    
     var selectedTag: Int = 1
+    
+    
+    // MARK: - Life Cycle
     
     override func configureView() {
         super.configureView()
-        
-        layer.cornerRadius = 4.0
-        layer.borderColor = AssetColors.gray300.cgColor
-        layer.borderWidth = 1.0
-        layer.masksToBounds = true
-        
-        addSubview(stackView)
-        
-        [oneStarBtn, twoStarBtn, threeStarBtn].forEach {
-            stackView.addArrangedSubview($0)
-            $0.titleLabel?.font = AssetFonts.subtitle2.font
-            $0.setTitleColor(AssetColors.gray500, for: .normal)
-            $0.setTitleColor(AssetColors.primary500, for: .highlighted)
-            $0.setTitleColor(AssetColors.primary500, for: [.selected, .highlighted])
-            $0.setTitleColor(AssetColors.primary500, for: .selected)
-            $0.setBackgroundColor(AssetColors.white, for: .normal)
-            $0.setBackgroundColor(AssetColors.primary50, for: .highlighted)
-            $0.setBackgroundColor(AssetColors.primary50, for: [.selected, .highlighted])
-            $0.setBackgroundColor(AssetColors.primary50, for: .selected)
-            $0.addTarget(self, action: #selector(toggleBtnTapped), for: .touchUpInside)
-        }
     
+        configureContentView()
+        configureBtn()
     }
     
     override func layoutView() {
         super.layoutView()
         
-        stackView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        
+        configureLayout()
     }
+    
+    
+    // MARK: - Functions
     
     @objc func toggleBtnTapped(_ sender: UIButton) {
         [oneStarBtn, twoStarBtn, threeStarBtn].forEach {
@@ -95,5 +85,54 @@ class ToggleButton: BaseView {
         }
     }
     
+    
+}
+
+
+// MARK: - Configure
+
+extension ToggleButton {
+    
+    private func configureContentView() {
+        layer.cornerRadius = 4.0
+        layer.borderColor = AssetColors.gray300.cgColor
+        layer.borderWidth = 1.0
+        layer.masksToBounds = true
+        
+        addSubview(stackView)
+    }
+    
+    private func configureBtn() {
+        [oneStarBtn, twoStarBtn, threeStarBtn].forEach {
+            stackView.addArrangedSubview($0)
+            
+            $0.titleLabel?.font = AssetFonts.subtitle2.font
+            
+            $0.setTitleColor(AssetColors.gray500, for: .normal)
+            $0.setTitleColor(AssetColors.gray500, for: .highlighted)
+            $0.setTitleColor(AssetColors.primary500, for: [.selected, .highlighted])
+            $0.setTitleColor(AssetColors.primary500, for: .selected)
+            
+            $0.setBackgroundColor(AssetColors.white, for: .normal)
+            $0.setBackgroundColor(AssetColors.white, for: .highlighted)
+            $0.setBackgroundColor(AssetColors.primary50, for: [.selected, .highlighted])
+            $0.setBackgroundColor(AssetColors.primary50, for: .selected)
+            
+            $0.addTarget(self, action: #selector(toggleBtnTapped), for: .touchUpInside)
+        }
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension ToggleButton {
+    
+    private func configureLayout() {
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
     
 }
