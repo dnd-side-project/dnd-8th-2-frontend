98323c1712867f2d9522b31d64ae39524f516190
kth1210 <k2hop1210@gmail.com>
2023-04-16 14:21:50 +0900

    [ADD] : Kingfisher 이용 이미지 캐싱 UIImageView+ 추가

---
+++ Reet-Place/Reet-Place/Global/Extension/UIImageView+.swift
@@ -0,0 +1,32 @@
+//
+//  UIImageView+.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 2023/04/16.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    
+    func setImage(with urlString: String) {
+        ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
+            switch result {
+            case .success(let value):
+                if let image = value.image {
+                    //캐시가 존재하는 경우
+                    self.image = image
+                } else {
+                    //캐시가 존재하지 않는 경우
+                    guard let url = URL(string: urlString) else { return }
+                    let resource = ImageResource(downloadURL: url, cacheKey: urlString)
+                    self.kf.setImage(with: resource)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+}
---
+++ Reet-Place/Reet-Place/Screen/BookMark/Model/BookmarkMainModel.swift
@@ -0,0 +1,11 @@
+//
+//  BookmarkMainModel.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 2023/04/16.
+//
+
+import Foundation
+
+struct BookmarkMainModel {
+}
