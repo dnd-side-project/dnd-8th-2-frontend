06a2d5ca3beb19939329531373918cedf66f9b1a
kth1210 <k2hop1210@gmail.com>
2023-04-24 18:12:48 +0900

    [FEAT] : 탈퇴 Description UI, 탈퇴하기 버튼 추가

--- Reet-Place/Reet-Place/Screen/UserInfo/ViewController/DeleteAccountVC.swift
+++ Reet-Place/Reet-Place/Screen/UserInfo/ViewController/DeleteAccountVC.swift
@@ -17,6 +17,45 @@ class DeleteAccountVC: BaseNavigationViewController {
     
     // MARK: - UI components
     
+    let descStackView = UIStackView()
+        .then {
+            $0.spacing = 12.0
+            $0.distribution = .fill
+            $0.alignment = .fill
+            $0.axis = .vertical
+        }
+    
+    let confirmTitle = BaseAttributedLabel(font: .subtitle1,
+                                           text: "탈퇴 신청 전 확인해주세요.",
+                                           alignment: .left,
+                                           color: AssetColors.primary500)
+    
+    let confirmDescView = UIView()
+        .then {
+            $0.backgroundColor = AssetColors.primary50
+        }
+    
+    let confirmDesc = BaseAttributedLabel(font: .body2,
+                                          text: "탈퇴 이후 회원 정보 및 이용기록은 모두 삭제되며,\n당신의 멋진 지도는 다시 복구할 수 없어요.",
+                                          alignment: .center,
+                                          color: AssetColors.gray700)
+        .then {
+            $0.numberOfLines = .zero
+        }
+    
+    let checkTitle = BaseAttributedLabel(font: .subtitle1,
+                                        text: "떠나시는 이유가 무엇인가요?",
+                                        alignment: .left,
+                                        color: AssetColors.black)
+    
+    let checkDesc = BaseAttributedLabel(font: .body2,
+                                        text: "더 나은 서비스 개선 목적의 자료로 사용할게요.",
+                                        alignment: .left,
+                                        color: AssetColors.gray500)
+    
+    let deleteBtn = ReetButton(with: "탈퇴하기",
+                               for: .outlined)
+    
     
     // MARK: - Variables and Properties
     
@@ -39,7 +78,7 @@ class DeleteAccountVC: BaseNavigationViewController {
     override func layoutView() {
         super.layoutView()
         
-        
+        configureLayout()
     }
     
     override func bindInput() {
@@ -69,7 +108,53 @@ extension DeleteAccountVC {
     }
     
     private func configureDeleteAccount() {
+        view.addSubviews([descStackView, deleteBtn])
         
+        [confirmTitle, confirmDescView, checkTitle, checkDesc].forEach {
+            descStackView.addArrangedSubview($0)
+        }
+        
+        confirmDescView.addSubview(confirmDesc)
     }
     
 }
+
+
+// MARK: - Layout
+
+extension DeleteAccountVC {
+    
+    private func configureLayout() {
+        descStackView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).offset(16.0)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20.0)
+            $0.height.equalTo(200.0)
+        }
+        
+        descStackView.setCustomSpacing(24.0, after: confirmDescView)
+        descStackView.setCustomSpacing(4.0, after: checkTitle)
+        
+        confirmTitle.snp.makeConstraints {
+            $0.height.equalTo(24.0)
+        }
+        
+        confirmDesc.snp.makeConstraints {
+            $0.center.equalTo(confirmDescView.snp.center)
+            $0.height.equalTo(42.0)
+        }
+        
+        checkTitle.snp.makeConstraints {
+            $0.height.equalTo(24.0)
+        }
+        
+        checkDesc.snp.makeConstraints {
+            $0.height.equalTo(21.0)
+        }
+        
+        deleteBtn.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide).inset(20.0)
+        }
+    }
+    
+}
+
