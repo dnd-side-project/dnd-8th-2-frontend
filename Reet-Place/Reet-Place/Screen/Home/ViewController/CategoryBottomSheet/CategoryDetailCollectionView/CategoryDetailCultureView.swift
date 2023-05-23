91e54751d080ed84ba840d1a1dd1a70169c16ba4
Kim HeeJae <iowa329@gmail.com>
2023-05-23 23:10:49 +0900

    [FEAT] : 장소목록 표시를 위한 카테고리 별 CategoryDetailView 구현

---
+++ Reet-Place/Reet-Place/Screen/Home/ViewController/CategoryBottomSheet/CategoryDetailCollectionView/CategoryDetailCultureView.swift
@@ -0,0 +1,48 @@
+//
+//  CategoryDetailCultureView.swift
+//  Reet-Place
+//
+//  Created by Kim HeeJae on 2023/05/07.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+import Then
+import SnapKit
+
+class CategoryDetailCultureView: CategoryDetailView {
+    
+    // MARK: - UI components
+    
+    // MARK: - Variables and Properties
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Functions
+    
+    override func bindCategoryDetailList(viewModel: CategoryFilterBottomSheetVM, bag: DisposeBag) {
+        let dataSource = RxCollectionViewSectionedReloadDataSource<CategoryDetailCultureDataSource> { _,
+            collectionView,
+            indexPath,
+            categoryType in
+
+            guard let cell = collectionView
+                .dequeueReusableCell(withReuseIdentifier: CategoryChipCVC.className,
+                                     for: indexPath) as? CategoryChipCVC else {
+                fatalError("Cannot deqeue cells named CategoryChipCVC")
+            }
+            cell.configureDetailPlaceCategoryChipCVC(category: categoryType.description)
+
+            return cell
+        }
+
+        viewModel.output.categoryDetailCultureDataSource
+            .bind(to: categoryDetailCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: bag)
+    }
+}
+
