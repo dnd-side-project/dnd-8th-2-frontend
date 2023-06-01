0ea19c39286f98ba1586a58082c7f0303197ffb2
kth1210 <k2hop1210@gmail.com>
2023-06-01 16:46:16 +0900

    [ADD] : ReetSelectBox VC 생성

---
+++ Reet-Place/Reet-Place/Screen/Shared/View/ReetSelectBox/ReetSelectBox.swift
@@ -0,0 +1,68 @@
+//
+//  ReetSelectBox.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 2023/06/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+import RxSwift
+import RxCocoa
+import RxGesture
+
+class ReetSelectBox: BaseViewController {
+    
+    // MARK: - UI components
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureContentView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension ReetSelectBox {
+    
+    private func configureContentView() {
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension ReetSelectBox {
+    
+    private func configureLayout() {
+        
+    }
+    
+}
