fa67e174a265a3499c31412bea7691e60e920871
kth1210 <k2hop1210@gmail.com>
2023-06-01 17:35:50 +0900

    [ADD] : SelectBoxTVC 생성

--- Reet-Place/Reet-Place/Screen/Shared/View/ReetSelectBox/ReetSelectBox.swift
+++ Reet-Place/Reet-Place/Screen/Shared/View/ReetSelectBox/ReetSelectBox.swift
@@ -18,10 +18,30 @@ class ReetSelectBox: BaseViewController {
     
     // MARK: - UI components
     
+    private let backgroundView: UIView = UIView()
+        .then {
+            $0.backgroundColor = .clear
+        }
+    
+    private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
+        .then {
+            $0.rowHeight = UITableView.automaticDimension
+            $0.layer.cornerRadius = 5.0
+            $0.isScrollEnabled = false
+            $0.separatorStyle = .none
+            $0.showsVerticalScrollIndicator = false
+        }
+    
     
     // MARK: - Variables and Properties
     
+    var location: CGRect?
     
+    var firstAction: (() -> Void)?
+    var secondAction: (() -> Void)?
+    var thirdAction: (() -> Void)?
+    
+
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -51,7 +71,11 @@ class ReetSelectBox: BaseViewController {
 extension ReetSelectBox {
     
     private func configureContentView() {
+        view.addSubviews([backgroundView, tableView])
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SelectBoxTVC.self, forCellReuseIdentifier: SelectBoxTVC.className)
     }
     
 }
@@ -62,7 +86,40 @@ extension ReetSelectBox {
 extension ReetSelectBox {
     
     private func configureLayout() {
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        tableView.snp.makeConstraints {
+            $0.height.equalTo(150.0)
+            $0.width.equalTo(100.0)
+            $0.top.equalToSuperview().offset((location?.origin.y ?? 0.0) + 50.0)
+            $0.trailing.equalToSuperview().offset(-(screenWidth - (location?.maxX ?? 0.0)))
+        }
     }
     
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension ReetSelectBox: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        54.0
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension ReetSelectBox: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SelectBoxTVC.className, for: indexPath) as? SelectBoxTVC else { fatalError("No such Cell") }
+        
+        return cell
+    }
 }
---
+++ Reet-Place/Reet-Place/Screen/Shared/View/ReetSelectBox/SelectBoxTVC.swift
@@ -0,0 +1,70 @@
+//
+//  SelectBoxTVC.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 2023/06/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class SelectBoxTVC: BaseTableViewCell {
+    
+    // MARK: - UI components
+    
+    private let selectLabel: BaseAttributedLabel = BaseAttributedLabel(font: .body2,
+                                                                       text: .empty,
+                                                                       alignment: .left,
+                                                                       color: AssetColors.black)
+    
+    
+    // MARK: - Variables and Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func configureView() {
+        super.configureView()
+        
+        configureContentView()
+    }
+    
+    override func layoutView() {
+        super.layoutView()
+        
+        configureLayout()
+    }
+    
+    // MARK: - Functions
+    
+}
+
+
+// MARK: - Configure
+
+extension SelectBoxTVC {
+    
+    private func configureContentView() {
+        contentView.addSubviews([selectLabel])
+        
+    }
+    
+}
+
+
+// MARK: - Layout
+
+extension SelectBoxTVC {
+    
+    private func configureLayout() {
+        selectLabel.snp.makeConstraints {
+            $0.height.equalTo(21.0)
+            $0.width.equalTo(96.0)
+            $0.edges.equalToSuperview()
+        }
+        
+    }
+    
+}
