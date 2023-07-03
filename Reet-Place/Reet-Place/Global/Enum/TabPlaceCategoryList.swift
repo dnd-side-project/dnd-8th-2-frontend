88d0645ab636885643c64c0aec0cfa135b7f3041
Kim HeeJae <iowa329@gmail.com>
2023-07-03 22:59:11 +0900

    [FEAT] : 장소검색 화면 구현
    - 검색 기록(카테고리 종류별 검색기록 항목 조회)
    - 검색 결과(북마크 TVC 공통기능 참조)

--- Reet-Place/Reet-Place/Global/Enum/TabPlaceCategoryList.swift
+++ Reet-Place/Reet-Place/Global/Enum/TabPlaceCategoryList.swift
@@ -1,5 +1,5 @@
 //
-//  CategoryBottomSheetTabItemList.swift
+//  TabPlaceCategoryList.swift
 //  Reet-Place
 //
 //  Created by Kim HeeJae on 2023/05/03.
@@ -12,6 +12,7 @@ import RxCocoa
 import RxDataSources
 
 enum TabPlaceCategoryList: String {
+    case all = "전체"
     case food = "식도락"
     case activity = "액티비티"
     case photoBooth = "포토부스"
@@ -32,11 +33,35 @@ extension TabPlaceCategoryList: CustomStringConvertible {
     }
 }
 
+extension TabPlaceCategoryList {
+    // TODO: - 카테고리별 검색기록 더미 데이터 삭제
+    var list: [String] {
+        switch self {
+        case .all:
+            return ["꾸꾸루꾸 치킨", "CGV 강남점", "반올림 피자샵", "어글리 베이커리", "시현하다 프레임", "인생다섯컷", "숏컷", "입구컷", "더현대", "판교현백", "행복한 백화점", "용산 메가박스"]
+        case .food:
+            return ["꾸꾸루꾸 치킨", "반올림 피자샵"]
+        case .activity:
+            return []
+        case .photoBooth:
+            return ["시현하다 프레임", "인생다섯컷", "숏컷", "입구컷"]
+        case .shopping:
+            return ["더현대", "판교현백", "행복한 백화점"]
+        case .cafe:
+            return ["어글리 베이커리"]
+        case .culture:
+            return ["용산 메가박스", "CGV 강남점"]
+        }
+    }
+}
+
 // MARK: - Functions
 
 extension TabPlaceCategoryList {
-    func createCategoryDetailView() -> CategoryDetailView {
+    func createCategoryDetailView() -> CategoryDetailView? {
         switch self {
+        case .all:
+            return nil
         case .food:
             return CategoryDetailFoodView(tabCategory: .food)
         case .activity:
