c43b9db5f6a115de6c82cb1c615ca2597d167043
Kim HeeJae <iowa329@gmail.com>
2023-07-04 20:56:27 +0900

    [CHORE] : 연관 URL 화살표 이미지 추가

--- Reet-Place/Reet-Place/Screen/BookMark/View/BookmarkCard/RelatedUrlButton.swift
+++ Reet-Place/Reet-Place/Screen/BookMark/View/BookmarkCard/RelatedUrlButton.swift
@@ -11,7 +11,16 @@ import Then
 import SnapKit
 
 class RelatedUrlButton: UIButton {
-        
+    
+    // MARK: - UI components
+    
+    private let baseStackView = UIStackView()
+        .then {
+            $0.axis = .horizontal
+            $0.distribution = .fill
+            $0.alignment = .center
+            $0.spacing = 8.0
+        }
     let urlLabel = BaseAttributedLabel(font: AssetFonts.caption,
                                           text: nil,
                                           alignment: .left,
@@ -19,33 +28,68 @@ class RelatedUrlButton: UIButton {
         .then {
             $0.lineBreakMode = .byTruncatingTail
         }
+    private let arrowRightImageView = UIImageView()
+        .then {
+            $0.image = AssetsImages.chevronRight
+        }
     
-        
+    // MARK: - Life Cycle
+    
     override init(frame: CGRect = .zero) {
-        
         super.init(frame: frame)
         
         configureButton()
+        configureLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Functions
+}
+
+// MARK: - Configure
+
+extension RelatedUrlButton {
     
     private func configureButton() {
+        isHidden = true
+        
         setBackgroundColor(AssetColors.gray100, for: .normal)
         setBackgroundColor(AssetColors.gray200, for: .highlighted)
         layer.cornerRadius = 2.0
+    }
+    
+}
+
+// MARK: - Layout
+
+extension RelatedUrlButton {
+    
+    private func configureLayout() {
+        // Add Subviews
+        addSubview(baseStackView)
+        [urlLabel, arrowRightImageView].forEach {
+            baseStackView.addArrangedSubview($0)
+        }
         
-        addSubview(urlLabel)
+        // Make Constraints
+        snp.makeConstraints  {
+            $0.height.equalTo(28.0)
+        }
+        
+        baseStackView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(8.0)
+        }
         
         urlLabel.snp.makeConstraints {
-            $0.centerY.equalTo(self.snp.centerY)
-            $0.leading.equalToSuperview().offset(8)
-            $0.trailing.equalToSuperview().offset(-8)
+            $0.height.equalTo(28.0)
+        }
+        arrowRightImageView.snp.makeConstraints {
+            $0.width.height.equalTo(16.0)
         }
     }
-
     
 }
