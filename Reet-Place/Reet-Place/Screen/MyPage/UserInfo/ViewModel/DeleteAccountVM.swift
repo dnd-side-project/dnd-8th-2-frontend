80d162d905d78f1633a0f7d5d9a052805a15ad76
kth1210 <k2hop1210@gmail.com>
2023-08-11 01:25:27 +0900

    [FEAT] : DeleteAccountVM -> 회원탈퇴 기능 네트워크 연결

--- Reet-Place/Reet-Place/Screen/MyPage/UserInfo/ViewModel/DeleteAccountVM.swift
+++ Reet-Place/Reet-Place/Screen/MyPage/UserInfo/ViewModel/DeleteAccountVM.swift
@@ -8,6 +8,8 @@
 import RxSwift
 import RxCocoa
 
+import Alamofire
+
 final class DeleteAccountVM: BaseViewModel {
     
     // MARK: - Variables and Properties
@@ -97,24 +99,61 @@ extension DeleteAccountVM: Output {
 
 extension DeleteAccountVM {
     
-    /// 릿플 서버에게 회원탈퇴를 요청
+    /// VC에서 회원탈퇴 요청
     func requestDeleteAccount() {
         let path = "/api/auth/unlink"
         let resource = URLResource<EmptyEntity>(path: path)
         let deleteAccountReason = createDeleteAccountRequestModel()
+        guard let identifier = KeychainManager.shared.read(for: .identifier) else { return }
         
-        // TODO: - 탈퇴 서버연결 마무리
-//        apiSession.reqeustPost(urlResource: resource, parameter: deleteAccountReason.parameter)
-//            .withUnretained(self)
-//            .subscribe(onNext: { owner, result in
-//                switch result {
-//                case .success:
-//
-//                case .failure(let error):
-//                    owner.apiError.onNext(error)
-//                }
-//            })
-//            .disposed(by: bag)
+        requestUnlink(urlResource: resource, parameter: deleteAccountReason.parameter, identifier: identifier)
+            .withUnretained(self)
+            .subscribe(onNext: { owner, result in
+                switch result {
+                case .success:
+                    print("탈퇴 성공")
+                    KeychainManager.shared.removeAllKeys()
+                    owner.output.isUnlinkSuccess.accept(true)
+                case .failure(let error):
+                    print("탈퇴 실패")
+                    print(error)
+                    owner.apiError.onNext(error)
+                }
+            })
+            .disposed(by: bag)
+    }
+    
+    /// 릿플 서버에 회원탈퇴 요청
+    func requestUnlink<T: Decodable>(urlResource: URLResource<T>, parameter: Parameters?, identifier: String) -> Observable<Result<T, APIError>> {
+        Observable<Result<T, APIError>>.create { observer in
+            var headers = HTTPHeaders()
+            headers.add(.accept("*/*"))
+            headers.add(.contentType("application/json"))
+            headers.add(name: "identifier", value: identifier)
+            
+            let task = AF.request(urlResource.resultURL,
+                                  method: .post,
+                                  parameters: parameter,
+                                  encoding: JSONEncoding.default,
+                                  headers: headers,
+                                  interceptor: AuthInterceptor())
+                .validate(statusCode: 200...399)
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        observer.onNext(.success(data))
+                        
+                    case .failure(let error):
+                        dump(error)
+                        guard let error = response.response else { return }
+                        observer.onNext(urlResource.judgeError(statusCode: error.statusCode))
+                    }
+                }
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
     
 }
