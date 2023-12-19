8b7947af5b56e37aca4a3737bd69dbee838a31da
Kim HeeJae <iowa329@gmail.com>
2023-12-19 23:03:54 +0900

    [CHORE] : 릿플 기본 FAB 쉐도우 설정 값 수정

--- Reet-Place/Reet-Place/Global/Extension/UIView+.swift
+++ Reet-Place/Reet-Place/Global/Extension/UIView+.swift
@@ -53,7 +53,7 @@ extension UIView {
     func addShadow(opacity: Float? = 0.4) {
         layer.shadowColor = CGColor(red: 23.0 / 255.0, green: 23.0 / 255.0, blue: 23.0 / 255.0, alpha: 1)
         layer.shadowOpacity = 0.4
-        layer.shadowRadius = 8.0
+        layer.shadowRadius = 4.0
         layer.shadowOffset = CGSize(width: 0, height: 2.0)
     }
     
