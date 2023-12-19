b79a2eab2d4b2ee8707d539679763ab2c47f308e
kth1210 <k2hop1210@gmail.com>
2023-12-19 14:01:52 +0900

    [FEAT] : BookmarkMap VC, VM 구현

---
+++ Reet-Place/Reet-Place/Screen/BookMark/Model/BookmarkSummaryModel.swift
@@ -0,0 +1,21 @@
+//
+//  BookmarkSummaryModel.swift
+//  Reet-Place
+//
+//  Created by 김태현 on 12/19/23.
+//
+
+import Foundation
+
+struct BookmarkSummaryModel {
+    let id: Int
+    let type: String
+    let name: String
+    let url: String
+    let category: String
+    let subCategory: String
+    let lotNumberAddress: String
+    let roadAddress: String
+    let lat: Double
+    let lng: Double
+}
