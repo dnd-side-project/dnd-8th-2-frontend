43887f1a6a1b67156a4ff66f61e1edc084aed30a
kth1210 <k2hop1210@gmail.com>
2024-02-13 14:47:11 +0900

    [FEAT] : PopUp에 goToLogin (로그인으로 이동) 추가

--- Reet-Place/Reet-Place/Global/Enum/PopUpType.swift
+++ Reet-Place/Reet-Place/Global/Enum/PopUpType.swift
@@ -10,6 +10,7 @@ import UIKit
 enum PopUpType {
     case deleteBookmark
     case withdrawal
+    case goToLogin
 }
 
 extension PopUpType {
@@ -19,6 +20,8 @@ extension PopUpType {
             return "북마크를 해제할까요?"
         case .withdrawal:
             return "정말 탈퇴하시겠어요?"
+        case .goToLogin:
+            return "로그인이 필요한 서비스에요!"
         }
     }
     
@@ -26,7 +29,7 @@ extension PopUpType {
         switch self {
         case .deleteBookmark:
             return AssetFonts.h4.font
-        case .withdrawal:
+        case .withdrawal, .goToLogin:
             return AssetFonts.subtitle1.font
         }
     }
@@ -37,6 +40,8 @@ extension PopUpType {
             return "북마크를 해제하시면,\n입력하셨던 내용이 전부 사라집니다."
         case .withdrawal:
             return "탈퇴 이후 당신의 장소들은\n다시 복구되지 않아요."
+        case .goToLogin:
+            return "로그인하고 장소들을 저장하고,\n나만의 지도를 채워보세요."
         }
     }
     
@@ -44,7 +49,7 @@ extension PopUpType {
         switch self {
         case .deleteBookmark:
             return AssetFonts.body2.font
-        case .withdrawal:
+        case .withdrawal, .goToLogin:
             return AssetFonts.body1.font
         }
     }
@@ -53,7 +58,7 @@ extension PopUpType {
         switch self {
         case .deleteBookmark:
             return AssetColors.error
-        case .withdrawal:
+        case .withdrawal, .goToLogin:
             return AssetColors.black
         }
     }
@@ -64,6 +69,8 @@ extension PopUpType {
             return "해제"
         case .withdrawal:
             return "탈퇴"
+        case .goToLogin:
+            return "로그인하기"
         }
     }
     
