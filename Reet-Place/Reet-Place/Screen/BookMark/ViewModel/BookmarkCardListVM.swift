c0de92b3e1f2a46e92582088d3aba92a20048e8a
kth1210 <k2hop1210@gmail.com>
2024-03-27 10:54:28 +0900

    [REFACTOR] : BookmarkCardListVM ReactorKit - Reactor 적용

--- Reet-Place/Reet-Place/Screen/BookMark/ViewModel/BookmarkCardListVM.swift
+++ Reet-Place/Reet-Place/Screen/BookMark/ViewModel/BookmarkCardListVM.swift
@@ -5,80 +5,129 @@
 //  Created by 김태현 on 2023/02/18.
 //
 
-import Foundation
 import RxSwift
 import RxCocoa
+import ReactorKit
 
-final class BookmarkCardListVM {
+final class BookmarkCardListVM: Reactor {
     
-    var input = Input()
-    var output = Output()
+    // MARK: - Properties
     
-    let network: NetworkProtocol = NetworkProvider()
-    let apiError = PublishSubject<APIError>()
-    private let type: BookmarkSearchType
-    
-    var bag = DisposeBag()
-    
-    struct Input {
-        var page: BehaviorRelay<Int> = BehaviorRelay(value: 0)
-        let size: Int = 20
+    enum Action {
+        case entering(BookmarkSearchType)
+        case deleteBookmark(index: Int)
+        case removeDeletedBookmark(id: Int)
+        case modifyBookmark(BookmarkInfo)
+        case nextPage
+        case togglingInfo(Int)
     }
     
-    struct Output {        
-        var bookmarkList: BehaviorRelay<Array<BookmarkCardModel>> = BehaviorRelay(value: [])
-        var isPaging: BehaviorRelay<Bool> = BehaviorRelay(value: false)
-        var isLastPage: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    enum Mutation {
+        case fetchBookmarkList([BookmarkCardModel], Bool)
+        case modifyBookmarkList([BookmarkCardModel])
+        case toggledInfo([BookmarkCardModel])
+        case isLoading(Bool)
+        case error
     }
+    
+    struct State {
+        var type: BookmarkSearchType
+        var page: Int = 0
+        var size: Int = 20
+        
+        var bookmarkList: [BookmarkCardModel] = []
+        var isPaging: Bool = false
+        var isLastPage: Bool = false
+    }
+    
+    let initialState: State
+    let network: NetworkProtocol
+    
+    
+    // MARK: - Initializer
     
     init(type: BookmarkSearchType) {
-        self.type = type
+        self.initialState = State(type: type)
+        self.network = NetworkProvider()
+    }
+    
+    
+    // MARK: - Mutate, Reduce
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .entering(let type):
+            return Observable.concat([
+                Observable.just(.isLoading(true)),
+                getBookmarkList(type: type),
+                Observable.just(.isLoading(false))
+            ])
+            
+        case .deleteBookmark(let index):
+            return deleteBookmark(index: index)
+            
+        case .removeDeletedBookmark(let id):
+            return deleteBookmark(id: id)
+            
+        case .modifyBookmark(let info):
+            return modifyBookmark(info: info)
+            
+        case .nextPage:
+            return Observable.concat([
+                Observable.just(.isLoading(true)),
+                getBookmarkList(type: currentState.type),
+                Observable.just(.isLoading(false))
+            ])
+            
+        case .togglingInfo(let index):
+            return toggleBookmarkInfo(index: index)
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
         
-        bindInput()
-        bindOutput()
+        switch mutation {
+        case .fetchBookmarkList(let bookmarkList, let isLastPage):
+            newState.page = state.page + 1
+            newState.bookmarkList = bookmarkList
+            newState.isLastPage = isLastPage
+            
+        case .modifyBookmarkList(let bookmarkList):
+            newState.bookmarkList = bookmarkList
+            
+        case .toggledInfo(let bookmarkList):
+            newState.bookmarkList = bookmarkList
+            
+        case .isLoading(let isPaging):
+            newState.isPaging = isPaging
+            
+        case .error:
+            print("Reactor Error")
+            break
+        }
+        
+        return newState
     }
-    
-    deinit {
-        bag = DisposeBag()
-    }
-    
 }
 
-
-// MARK: - Input
-
-extension BookmarkCardListVM: Input {
-    
-    func bindInput() {
-        
-    }
-    
-}
-
-
-// MARK: - Output
-
-extension BookmarkCardListVM: Output {
-    
-    func bindOutput() {
-        
-    }
-    
-}
 
 // MARK: - Functions
 
 extension BookmarkCardListVM {
     
-    func deleteBookmark(id: Int) {
-        let originBookmarkList = output.bookmarkList.value
+    func deleteBookmark(id: Int) -> Observable<Mutation> {
+        let originBookmarkList = currentState.bookmarkList
         let deletedBookmarkList = originBookmarkList.filter({ $0.id != id })
-        output.bookmarkList.accept(deletedBookmarkList)
+        
+        return Observable.just(.modifyBookmarkList(deletedBookmarkList))
     }
     
-    func modifyBookmark(info: BookmarkInfo) {
+    func modifyBookmark(info: BookmarkInfo) -> Observable<Mutation> {
         let card = info.toBookmarkCardModel()
-        var currentBookmarkList = output.bookmarkList.value
+        let type = currentState.type
+        var currentBookmarkList = currentState.bookmarkList
+        
         switch type {
         case .all:
             currentBookmarkList = currentBookmarkList.map { $0.id == card.id ? card : $0 }
@@ -87,10 +136,17 @@ extension BookmarkCardListVM {
                 currentBookmarkList = currentBookmarkList.map { $0.id == card.id ? card : $0 }
             } else {
                 currentBookmarkList = currentBookmarkList.filter { $0.id != card.id }
-                
             }
         }
-        output.bookmarkList.accept(currentBookmarkList)
+        
+        return Observable.just(.modifyBookmarkList(currentBookmarkList))
+    }
+    
+    func toggleBookmarkInfo(index: Int) -> Observable<Mutation> {
+        var bookmarkList = currentState.bookmarkList
+        bookmarkList[index].infoHidden.toggle()
+        
+        return Observable.just(.toggledInfo(bookmarkList))
     }
     
 }
@@ -99,55 +155,47 @@ extension BookmarkCardListVM {
 
 extension BookmarkCardListVM {
     
-    func getBookmarkList(type: BookmarkSearchType) {
-        let page = input.page.value
+    func getBookmarkList(type: BookmarkSearchType) -> Observable<Mutation> {
+        let page = currentState.page
         let path = "/api/bookmarks?searchType=\(type.rawValue)&page=\(page)&size=10&sort=LATEST"
         let endPoint = EndPoint<BookmarkListResponseModel>(path: path, httpMethod: .get)
         
-        output.isPaging.accept(true)
-        network.request(with: endPoint)
+        return network.request(with: endPoint)
             .withUnretained(self)
-            .subscribe(onNext: { owner, result in
+            .map { owner, result -> Mutation in
                 switch result {
                 case .success(let data):
                     let bookmarkList = data.content.map { $0.toBookmarkCardModel() }
-                    let originBookmarkList = owner.output.bookmarkList.value
-                    
-                    owner.output.isLastPage.accept(data.last)
-                    owner.input.page.accept(page + 1)
+                    let originBookmarkList = owner.currentState.bookmarkList
+                    let isLastPage = data.last
+
                     if page == 0 {
-                        owner.output.bookmarkList.accept(bookmarkList)
+                        return .fetchBookmarkList(bookmarkList, isLastPage)
                     } else {
-                        owner.output.bookmarkList.accept(originBookmarkList + bookmarkList)
+                        return .fetchBookmarkList(originBookmarkList + bookmarkList, isLastPage)
                     }
-                case .failure(let error):
-                    owner.apiError.onNext(error)
+                case .failure:
+                    return .error
                 }
-                owner.output.isPaging.accept(false)
-            })
-            .disposed(by: bag)
+            }
     }
     
-    func deleteBookmark(index: Int) {
-        let id = output.bookmarkList.value[index].id
+    func deleteBookmark(index: Int) -> Observable<Mutation> {
+        let id = currentState.bookmarkList[index].id
         let path = "/api/bookmarks/\(id)"
         let endPoint = EndPoint<EmptyEntity>(path: path, httpMethod: .delete)
         
-        network.request(with: endPoint)
+        return network.request(with: endPoint)
             .withUnretained(self)
-            .subscribe(onNext: { owner, result in
+            .map { owner, result -> Mutation in
                 switch result {
                 case .success:
-                    let deletedBookmarkList = owner
-                        .output.bookmarkList.value
-                        .filter({ $0.id != id })
-                    owner.output.bookmarkList.accept(deletedBookmarkList)
-                case .failure(let error):
-                    print(error)
-                    owner.apiError.onNext(error)
+                    let deletedBookmarkList = owner.currentState.bookmarkList.filter({ $0.id != id })
+                    return .modifyBookmarkList(deletedBookmarkList)
+                case .failure:
+                    return .error
                 }
-            })
-            .disposed(by: bag)
+            }
     }
     
 }
